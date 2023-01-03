5e94ec2da2ed57a58f03fc82fc78b1aa0059af4f
Marloes van der Zalm <marloesvdzalm@gmail.com>
2023-01-03 15:14:18 +0100

    contact form toegevoegd

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -15,6 +15,22 @@ const Contact = ({data: {wpPage: {contactPageFields}}}) => {
                     __html: contactPageFields.description
                 }}/>
             </article>
+            <section>
+                <h4>Contact Us</h4>
+                <form name="contact" method="POST" data-netlify="true">
+                    <label>Your Fist Name:</label>
+                    <input type="text" name="firstname" required={true} />
+                    <label>Your Last Name</label>
+                    <input type="text" name="lastname" required={true} />
+                    <label>Your Email:</label>
+                    <input type="email" name="email" required={true} />
+                    <label>Subject</label>
+                    <input type="text" name="subject" required={true} />
+                    <label>Message:</label>
+                    <textarea name="message" required={true}></textarea>
+                    <button type="submit">Send</button>
+                </form>
+            </section>
         </Layout>
     )
 }
